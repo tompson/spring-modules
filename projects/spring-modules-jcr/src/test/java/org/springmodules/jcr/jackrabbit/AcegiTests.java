b65d1b521ff923ba6a6783a005d961187a015724
tompson <tom@einwaller.net>
2009-08-10 10:52:09 +0200

    adapted to maven compliant structure

---
+++ projects/spring-modules-jcr/src/test/java/org/springmodules/jcr/jackrabbit/AcegiTests.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springmodules.jcr.jackrabbit;
+
+import java.io.IOException;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.context.SecurityContextHolder;
+import org.springframework.security.providers.TestingAuthenticationToken;
+import org.springframework.test.AbstractTransactionalSpringContextTests;
+import org.springmodules.jcr.JcrCallback;
+import org.springmodules.jcr.JcrTemplate;
+
+/**
+ * @author Costin Leau
+ * 
+ */
+public class AcegiTests extends AbstractTransactionalSpringContextTests {
+
+	private JcrTemplate template;
+
+	@Override
+	protected String[] getConfigLocations() {
+		return new String[] { "/org/springmodules/jcr/jackrabbit/acegi-context.xml" };
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.test.AbstractTransactionalSpringContextTests#onSetUpBeforeTransaction()
+	 */
+	@Override
+	protected void onSetUpBeforeTransaction() throws Exception {
+		SecurityContextHolder.getContext().setAuthentication(
+				new TestingAuthenticationToken(new Object(), new Object(), new GrantedAuthority[] {}));
+
+	}
+
+	public void testWriteRights() {
+		template.execute(new JcrCallback() {
+
+			public Object doInJcr(final Session session) throws IOException, RepositoryException {
+				final Node rootNode = session.getRootNode();
+				final Node one = rootNode.addNode("bla-bla-bla");
+				one.setProperty("some prop", false);
+				final Node two = one.addNode("foo");
+				two.setProperty("boo", "hoo");
+				final Node three = two.addNode("bar");
+				three.setProperty("whitehorse", new String[] { "super", "ultra", "mega" });
+				session.save();
+				return null;
+			}
+		});
+	}
+
+	public void setTemplate(final JcrTemplate template) {
+		this.template = template;
+	}
+}
